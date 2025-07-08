c3b802c5a50e1694110f8dd5791f9b3036c8284f
yassinsl <slawiyassin152@gmail.com>
2025-07-08 10:29:20 -0700

    incomplete work :)

--- load_vedio.c
+++ load_vedio.c
@@ -14,7 +14,8 @@ int main()
     AVFormatContext *info_file;
     AVCodec *codec;
     SDL_Window *w;
-
+    AVPacket *pack;
+    AVFrame *frame = av_frame_alloc(), *rgb_frame = av_frame_alloc();
     info_file = NULL;
     if (avformat_open_input(&info_file, PATH, NULL, NULL) < 0)
         return (1);
@@ -27,13 +28,27 @@ int main()
      avcodec_parameters_to_context(dec_ctx, info_file->streams[0]->codecpar);
   if(avcodec_open2(dec_ctx, codec, NULL) < 0) return(1);
   if(SDL_Init(SDL_INIT_VIDEO) < 0) return 1;
-  while(1)
       w = SDL_CreateWindow(
                   "Hi",
                    SDL_WINDOWPOS_CENTERED,
                    SDL_WINDOWPOS_CENTERED,
                     1600,
                     1200,
-                    SDL_WINDOW_SHOWN
-                  );
+                    SDL_WINDOW_SHOWN);
+      pack = av_packet_alloc();
+    if(!pack) return 1;
+    while(!(av_read_frame(info_file, pack) < 0))
+  {
+    if(pack->stream_index == vedio_stream_index)
+    {
+           avcodec_send_packet(dec_ctx, packet);
+            while(!avcodec_receive_frame(packet, frame))
+            {
+                  sws_scale(dec_ctx,
+                frame->data, frame->linesize,
+                0, frame->height,
+                    rgb_frame->data, rgb_frame->linesize);
+            }
+    }
+  }
 }
